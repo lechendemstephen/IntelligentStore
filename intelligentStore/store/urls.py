c5d6ba23afdde2e34b83620d1b850e50296444bb
lechendemstephen <51188322+lechendemstephen@users.noreply.github.com>
2025-01-10 13:11:30 -0500

    completed the projects templating

--- intelligentStore/store/urls.py
+++ intelligentStore/store/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path 
 from . import views
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='home'), 
+    path('shop/', views.shop, name='shop'), 
+    path('order/', views.orders, name='order'),
+    path('account/', views.account, name='account'),
+    path('cart/', views.cart, name='cart'),
+
+    # authentication 
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+
+
+
 ]
--- intelligentStore/store/views.py
+++ intelligentStore/store/views.py
@@ -2,6 +2,47 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
+# function for the homepage 
 def index(request): 
 
     return render(request, 'pages/public/index.html')
+
+
+# shop preview 
+def shop(request): 
+
+    return render(request, 'pages/public/shop.html')
+
+
+# orders preview
+def orders(request): 
+
+    return render(request, 'pages/public/orders.html')
+
+
+# account preview
+def account(request): 
+
+    return render(request, 'pages/account/account_preview.html')
+
+# cart preview
+def cart(request): 
+
+    return render(request, 'pages/public/cart.html')
+
+
+
+# authentication 
+
+
+
+def signup(request): 
+
+    return render(request, 'pages/authentication/signup.html')
+
+
+
+def login(request): 
+
+    return render(request, 'pages/authentication/login.html')
